7e58a1931422b246ec9034d2cac1169285c8cc34
Maggie447 <mserrat1@student.fitchburgstate.edu>
2024-12-04 00:20:15 -0500

    started working on player movement/camera rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
