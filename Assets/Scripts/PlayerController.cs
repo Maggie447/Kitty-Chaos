3df3be78edd876f1655c8426c53dc559ebf08166
Maggie447 <mserrat1@student.fitchburgstate.edu>
2024-12-04 10:25:11 -0500

    worked on UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
 
