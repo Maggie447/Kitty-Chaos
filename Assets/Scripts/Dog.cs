ad5f61f82dc96c6189d9ba551d810cd81e12d4e9
Maggie447 <mserrat1@student.fitchburgstate.edu>
2024-12-16 23:35:24 -0500

    continued dog movement script

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Dog : MonoBehaviour
 {
+    public float speed = 3.0f;
+    private Rigidbody dogRb;
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dogRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("cat placeholder");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        dogRb.AddForce(lookDirection * speed);
     }
 }
